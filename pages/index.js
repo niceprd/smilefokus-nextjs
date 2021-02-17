0881e1a9b8a43618d89bb1ee7f4e0c7c057ab275
Phamin Promcharoen <nice.phamin@gmail.com>
2021-02-18 01:49:24 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Homepage from "./homepage";
+import React from "react";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Homepage />
+    </React.Fragment>
+  );
+}
